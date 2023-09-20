4cf0e6c8247b1eaf0c44cd5c8e9ac013e3c8de71
123Ayomi <otepolabusayo@yahoo.com>
2023-09-20 03:43:34 -0700

    getenv.c

---
+++ getenv.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+* get_environ -  It returns the str array copy of our environ
+* @info: a struct containing potential args. Used to maintain
+*          constant func. prototype.
+* Return: Always 0
+*/
+char **get_environ(info_t *info)
+{
+if (!info->environ || info->env_changed)
+{
+info->environ = list_to_strings(info->env);
+info->env_changed = 0;
+}
+
+return (info->environ);
+}
+
+/**
+* _unsetenv - It removes an envir variable
+* @info: a structure containing potential args. Used to maintain
+*        constant func prototype.
+*  Return: 1 on delete, 0 otherwise
+* @var: the str env var property
+*/
+int _unsetenv(info_t *info, char *var)
+{
+list_t *node = info->env;
+size_t k = 0;
+char *p;
+
+if (!node || !var)
+return (0);
+
+while (node)
+{
+p = starts_with(node->str, var);
+if (p && *p == '=')
+{
+info->env_changed = delete_node_at_index(&(info->env), i);
+k = 0;
+node = info->env;
+continue;
+}
+node = node->next;
+k++;
+}
+return (info->env_changed);
+}
+
+/**
+* _setenv - It Initialize a new envir variable,
+*             or modify an existing one
+* @info: a struct containing potential arguments. Used to maintain
+*        const function prototype.
+* @var: the strs env var property
+* @value: the strs env var value
+*  Return: Always 0
+*/
+int _setenv(info_t *info, char *var, char *value)
+{
+char *buf = NULL;
+list_t *node;
+char *p;
+
+if (!var || !value)
+return (0);
+
+buf = malloc(_strlen(var) + _strlen(value) + 2);
+if (!buf)
+return (1);
+_strcpy(buf, var);
+_strcat(buf, "=");
+_strcat(buf, value);
+node = info->env;
+while (node)
+{
+p = starts_with(node->str, var);
+if (p && *p == '=')
+{
+free(node->str);
+node->str = buf;
+info->env_changed = 1;
+return (0);
+}
+node = node->next;
+}
+add_node_end(&(info->env), buf, 0);
+free(buf);
+info->env_changed = 1;
+return (0);
+}
